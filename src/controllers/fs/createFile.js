39496854d58530ca652ea142aad26e40c617998e
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2022-12-19 17:33:02 +0500

    refactor: add current path in create file

--- src/controllers/fs/createFile.js
+++ src/controllers/fs/createFile.js
@@ -4,7 +4,10 @@ import {createWriteStream} from 'fs'
 export function createFile(dirname, str) {
     const fileName = str.slice(4);
     try {
-        const source = join(dirname, fileName);
+        const currentFileName = fileName.split('\/');
+        console.log(currentFileName);
+        const fileNameWithFormat = currentFileName[currentFileName.length - 1]
+        const source = join(dirname, fileNameWithFormat);
         const writable = createWriteStream(source);
         console.log(`${fileName} has created!`);
         
